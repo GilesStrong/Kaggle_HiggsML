5162215279a96ab0b187eb42522c4123fcca89fc
GilesStrong <giles.strong@outlook.com>
2018-03-16 17:10:19 +0000

    Day4

--- Modules/Basics.py
+++ Modules/Basics.py
@@ -14,6 +14,9 @@ sns.set_style("white")
 from ML_Tools.Plotting_And_Evaluation.Plotters import *
 from ML_Tools.General.Misc_Functions import *
 from ML_Tools.General.Ensemble_Functions import ensemblePredict
+from ML_Tools.General.Batch_Train import getFeature
+
+from sklearn.model_selection import StratifiedKFold
 
 dirLoc = "../Data/"
 
@@ -33,16 +36,32 @@ def AMS(s, b):
         return math.sqrt(radicand)
 
 
-def amsScan(inData):
+def amsScan(inData, scale=False):
     best = [0,-1]
     ams = []
     for index, row in inData.iterrows():
-        ams.append(AMS(np.sum(inData.loc[(inData['pred_class'] >= row['pred_class']) & (inData['gen_target'] == 1), 'gen_weight']),
-                  np.sum(inData.loc[(inData['pred_class'] >= row['pred_class']) & (inData['gen_target'] == 0), 'gen_weight'])))
+        s = np.sum(inData.loc[(inData['pred_class'] >= row['pred_class']) & (inData['gen_target'] == 1), 'gen_weight'])
+        b = np.sum(inData.loc[(inData['pred_class'] >= row['pred_class']) & (inData['gen_target'] == 0), 'gen_weight'])
+        if scale:
+            s *= scale[0]/np.sum(inData[(inData['gen_target'] == 1)]['gen_weight'])
+            b *= scale[1]/np.sum(inData[(inData['gen_target'] == 0)]['gen_weight'])
+        ams.append(AMS(s, b))
         if ams[-1] > best[1]:
             best = [row['pred_class'], ams[-1]]
     print best
-    return ams
+    return ams, best
+
+def foldAMSScan(inData, N=10):
+    kf = StratifiedKFold(n_splits=N, shuffle=True)
+    folds = kf.split(inData, inData['gen_target'])
+    bests = []
+    for i, (train, test) in enumerate(folds):
+        bests.append(amsScan(inData.iloc[test], (np.sum(inData[(inData['gen_target'] == 1)]['gen_weight']), np.sum(inData[(inData['gen_target'] == 0)]['gen_weight'])))[1])
+        print "Fold {}, best AMS {} at cut of {}. Total weights Signal:Bkg. {}:{}".format(i, bests[-1][1], bests[-1][0],
+                                                                                          np.sum(inData.iloc[test][inData.gen_target == 1]['gen_weight']),
+                                                                                          np.sum(inData.iloc[test][inData.gen_target == 0]['gen_weight']))
+    print "Mean cut", np.average([x[0] for x in bests], weights=[1/x[1] for x in bests]), "mean AMS", np.average([x[1] for x in bests], weights=[1/x[1] for x in bests])
+    return bests
 
 def scoreTest(ensemble, weights):
     testData = h5py.File(dirLoc + 'testing.hdf5', "r+")
@@ -64,3 +83,11 @@ def saveTest(cut, name):
 
     print dirLoc + name + '_test.csv'
     data.to_csv(dirLoc + name + '_test.csv', columns=['EventId', 'RankOrder', 'Class'], index=False)
+
+def convertToDF(datafile, columns={'gen_target', 'gen_weight', 'pred_class'}, nLoad=-1):
+    data = pandas.DataFrame()
+    data['gen_target'] = getFeature('targets', datafile, nLoad)
+    data['gen_weight'] = getFeature('weights', datafile, nLoad)
+    data['pred_class'] = getFeature('pred', datafile, nLoad)
+    print len(data), "candidates loaded"
+    return data
